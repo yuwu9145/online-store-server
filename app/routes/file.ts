db81607bd5700b70c3c98a874391ddb26f79c4eb
yuchaosydney <yuchao.sydneyuni@gmail.com>
2018-04-07 17:11:03 +1000

    file upload with timestamp to make the filename unique

--- app/routes/file.ts
+++ app/routes/file.ts
@@ -2,11 +2,15 @@
 
 import * as express from 'express';
 import * as fileHelper from '../helpers/fileHelper';
+import * as path from 'path';
 
 namespace Route {
     export class Routes {
         public upload(req: express.Request, res: express.Response, next: express.NextFunction) {
-            fileHelper.saveFile(req.file.originalname, req.file.buffer).subscribe(
+            let filename: string = req.file.originalname;
+            const fileExtension: string = path.extname(filename);
+            filename = `${filename.replace(fileExtension, '')}_${new Date().getTime()}` + fileExtension;
+            fileHelper.saveFile(filename, req.file.buffer).subscribe(
                 x => console.log('saving file observable onNext: %s', x),
                 e => res.json({
                     success: false,
