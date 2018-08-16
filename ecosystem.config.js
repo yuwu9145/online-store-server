65079d8b885a4ed8ecef0783edb990103e9c9eb3
Yuchao Wu <yuchao@mylocalbroker.com.au>
2018-08-17 09:53:45 +1000

    change config for new dev env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,14 +12,14 @@ module.exports = {
       env: {
         NODE_ENV: 'development',
         PORT: 3000,
-        MONGODB_CONNECTION: 'mongodb://13.210.159.104/phonecasestore',
+        MONGODB_CONNECTION: 'mongodb://localhost/phonecasestore',
         TOKEN_SECRET: 'ilovescotchyscotch',
         UPLOAD_FILES_PATH: './dist/uploads'
       },
       env_production : {
         NODE_ENV: 'production',
-        PORT: 3000,
-        MONGODB_CONNECTION: 'mongodb://13.210.159.104/phonecasestore',
+        PORT: 8080,
+        MONGODB_CONNECTION: 'mongodb://localhost/phonecasestore',
         TOKEN_SECRET: 'ilovescotchyscotch',
         UPLOAD_FILES_PATH: './dist/uploads'
       }
